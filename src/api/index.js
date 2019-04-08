09c73c6cdd99fb88173d958d2a6b44685b1e13e1
WangLi <wangli@qq.com>
2019-04-08 08:34:55 +0800

    '完成添加分类功能，修改名称功能以及查看其子品类功能'

--- src/api/index.js
+++ src/api/index.js
@@ -1,21 +1,6 @@
 /**
  * 包含n个请求函数模块
  */
-/*
-  服务器代理模式：
-    1. 作用：解决开发环境下的跨域问题。生产环境不能使用
-    2. 配置：
-      在package.json中添加 "proxy": "目标服务器地址"
-    3. 工作原理
-      原因：客户端和服务端有跨域问题，而服务端和服务端之间是没有跨域问题的
-      解决：
-        让开发（代理）服务器运行当前项目。项目和开发服务器就不会有跨域问题（遵循了同源策略）
-        在让开发（代理）服务器去访问目标服务器，（服务端和服务端之间是没有跨域问题的），
-        所以开发（代理）服务器将请求目标服务器的资源返回当前页面
-
-   什么时候需要重启服务器：
-    修改了webpack配置就需要重启，才能生效
- */
 import jsonp from 'jsonp';
 import ajax from './ajax';
 
@@ -42,3 +27,12 @@ export const reqWeather = (city) => {
         )
     })
 }
+
+//请求分类列表数据
+export const reqGetCategories=(parentId)=>ajax(prefix+'/manage/category/list',{parentId});
+
+// 请求添加分类函数
+export const reqAddCategory = (parentId, categoryName) => ajax(prefix + '/manage/category/add', {parentId, categoryName}, 'POST');
+
+// 请求修改分类名称函数
+export const reqUpdateCategoryName = (categoryId, categoryName) => ajax(prefix + '/manage/category/update', {categoryId, categoryName}, 'POST');
